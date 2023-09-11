88987b290c64ea825160c830d51220a49d8abc8b
Daniel Souza <danielrsouza10@gmail.com>
2023-09-11 09:54:13 -0300

    components created

---
+++ src/app/components/news-title/news-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-title',
+  templateUrl: './news-title.component.html',
+  styleUrls: ['./news-title.component.css']
+})
+export class NewsTitleComponent {
+
+}
